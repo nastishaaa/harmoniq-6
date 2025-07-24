fc3a161e32d096dd6fa07a02e7f7a4cec561bc75
Eugene <y.holubnychyi@gmail.com>
2025-07-24 20:59:38 -0230

    base structure for user endpoints

---
+++ src/controllers/user.js
@@ -0,0 +1,5 @@
+export const getCurrentUser = async (req, res) => {};
+export const getSavedArticles = async (req, res) => {};
+export const getCreatedArticles = async (req, res) => {};
+export const addSavedArticle = async (req, res) => {};
+export const removeSavedArticle = async (req, res) => {};
--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import pino from 'pino-http';
 import cors from 'cors';
-import dotenv from "dotenv";
+import dotenv from 'dotenv';
 // import router from './routers/index.js';
 import { notFoundHandler } from './middlewares/notFoundHandler.js';
 import { errorHandler } from './middlewares/errorHandler.js';
@@ -13,28 +13,28 @@ dotenv.config();
 const PORT = Number(process.env.PORT) || 3000;
 
 export const startServer = async () => {
-    const app = express();
+  const app = express();
 
-    app.use(express.json());
-    app.use(cors());
-    app.use(cookieParser());
+  app.use(express.json());
+  app.use(cors());
+  app.use(cookieParser());
 
-    app.use(
-        pino({
-            transport: {
-                target: 'pino-pretty',
-            },
-        }),
-    );
+  app.use(
+    pino({
+      transport: {
+        target: 'pino-pretty',
+      },
+    }),
+  );
 
-    // app.use('/api-docs', swaggerServe, swaggerDocs);
+  // app.use('/api-docs', swaggerServe, swaggerDocs);
 
-    // app.use('/', router);
+  // app.use('/', router);
 
-    app.use(notFoundHandler);
-    app.use(errorHandler);
+  app.use(notFoundHandler);
+  app.use(errorHandler);
 
-    app.listen(PORT, () => {
-        console.log(`Server is running on port ${PORT}`);
-    });
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
 };
