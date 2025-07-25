98561d46da6692503018562122ff7aed3b5c2356
Yevhenii Holubnychyi <y.holubnychyi@gmail.com>
2025-07-25 09:53:24 -0230

    base structure for user endpoints (#1)

--- src/server.js
+++ src/server.js
@@ -13,28 +13,27 @@ dotenv.config();
 const PORT = Number(process.env.PORT) || 3000;
 
 export const startServer = async () => {
-    const app = express();
+  const app = express();
 
-    app.use(express.json());
-    app.use(cors());
-    app.use(cookieParser());
+  app.use(express.json());
+  app.use(cors());
+  app.use(cookieParser());
 
-    app.use(
-        pino({
-            transport: {
-                target: 'pino-pretty',
-            },
-        }),
-    );
+  app.use(
+    pino({
+      transport: {
+        target: 'pino-pretty',
+      },
+    }),
+  );
 
-    // app.use('/api-docs', swaggerServe, swaggerDocs);
-
+  // app.use('/api-docs', swaggerServe, swaggerDocs);
     app.use('/', router);
 
-    app.use(notFoundHandler);
-    app.use(errorHandler);
+  app.use(notFoundHandler);
+  app.use(errorHandler);
 
-    app.listen(PORT, () => {
-        console.log(`Server is running on port ${PORT}`);
-    });
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
 };
