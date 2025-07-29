e1b45efdc12dbd1aa470e1ac3834dabbdc0401f2
Anastasiia <anastasiiprfnk@gmail.com>
2025-07-29 13:07:50 +0300

    resolve conflicts

--- src/server.js
+++ src/server.js
@@ -35,7 +35,5 @@ export const startServer = async () => {
 
   app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
-    console.log(`My Swagger:       http://localhost:${PORT}/api-docs`);
-    console.log(`Team Swagger:       http://localhost:${PORT}/docs`);
   });
 };
