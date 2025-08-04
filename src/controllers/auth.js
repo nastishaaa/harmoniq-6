cb1b73227520963796afa1529c78b8d178bf33a6
Anastasiia <anastasiiprfnk@gmail.com>
2025-08-04 19:35:46 +0300

    cors changes

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -11,13 +11,13 @@ import {
 const setupSessionCookies = (session, res) => {
   res.cookie('sessionId', session._id, {
     httpOnly: true,
-    secure: false,                
+    secure: true,                
     sameSite: 'None',           
     expires: session.refreshTokenValidUntil,
   });
   res.cookie('refreshToken', session.refreshToken, {
     httpOnly: true,
-    secure: false,                
+    secure: true,                
     sameSite: 'None',            
     expires: session.refreshTokenValidUntil,
   });
