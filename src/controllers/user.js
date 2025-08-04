2a932b24a5dbf5b275173461d9b1f510f606b9ee
Eugene <y.holubnychyi@gmail.com>
2025-08-04 17:25:35 -0230

    видалення дублювання коду

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -33,10 +33,8 @@ export const removeSavedArticle = async (req, res) => {
 
 export const getAllAuthors = async (req, res) => {
   const { page = 1, perPage = 20 } = req.query;
+  const result = await userService.getAllAuthors(Number(page), Number(perPage));
 
-  const result = await userService.getAllAuthors(Number(page), Number(perPage));
-  const { page = 1, perPage = 20 } = req.query;
-  const result = await userService.getAllAuthors(Number(page), Number(perPage));
   res.status(200).json({
     status: 200,
     message: 'Successfully fetched authors!',
